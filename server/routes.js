32ba5b9c67ac642d995c8866ad5e017382dc999f
Isidro Martínez <isidro.amv@gmail.com>
2015-05-13 19:36:13 -0500

    cambio de puerto de cords

--- server/routes.js
+++ server/routes.js
@@ -32,7 +32,7 @@ module.exports = function(app) {
 
   app.use(function  (req, res, next) {
       // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:9000');
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8100');
       // Request methods you wish to allow
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS');
       // Request headers you wish to allow
