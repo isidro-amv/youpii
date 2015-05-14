f4f2c4d8b404a05432180376e1d019ecacd2e085
Isidro Martínez <isidro.amv@gmail.com>
2015-05-13 19:26:44 -0500

    Soporte de cords

--- server/routes.js
+++ server/routes.js
@@ -29,4 +29,22 @@ module.exports = function(app) {
     .get(function (req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
+
+  app.use(function  (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:9000');
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS');
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, accept');
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      // Pass to next layer of middleware
+      next();
+    })
+    .options('/*', function (req, res) {
+      res.statusCode = 200;
+      res.json({status:"OK"});
+    });
 };
